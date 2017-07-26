af8d7f500ac32f051d45b596513ccae459f9cfc7
Pramuka Perera <pramuka.perera@gmail.com>
2017-07-25 22:12:16 -0400

    Assembler can now separate code into group labels and (opcodes + operands), and can throw out the comments.

--- assembler_8080.c
+++ assembler_8080.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <string.h>
 
+#define	NEWLINE	0x0a
+
 typedef struct instruction_pair
 {
 	char* mnemonic;
@@ -268,27 +270,34 @@ typedef struct list_node
 }
 node;
 
-static node labels;
-
-node *AddNode(char* new_label, uint16_t new_address, node* head)
+void AddLabelNode(char *new_label, uint16_t new_address, node ** head)
 {
-	node *new_node = malloc(sizeof(node));
+	node 	*new_node;
 
-	//
-	strncpy(new_node -> label, new_label, sizeof(new_node -> label));
+	if((new_node = malloc(sizeof(node))) == NULL)
+	{
+		printf("Couldn't allocate space to save a label node.");
+		exit(0);
+	}
+
+	strncpy(new_node -> label, new_label, sizeof(new_node -> label));	
 	new_node -> address = new_address;
 	new_node -> next_node = NULL;
 	new_node -> last_node = NULL;	
 
-	/*
-	head -> last_node -> next_node = new_node;
-	head -> last_node = new_node;
-	*/
+	if(*head == NULL) 
+	{
+		(*head) = new_node;
+		(*head) -> last_node = *head;
 
-	return new_node;
+		return;
+	}
+
+	((*head) -> last_node) -> next_node = new_node;
+	(*head) -> last_node = new_node;
 }
 
-uint32_t FindAddress(char *label_of_address, node *head)
+uint32_t FindLabelValue(char *label_of_address, node *head)
 {
 	node *current_node = head;
 	
@@ -318,29 +327,37 @@ void FreeLabelList(node *head)
 	}
 }
 
+typedef struct line_token
+{
+	char *token;
+	int token_size;
+}
+token;
+
 
 
 FILE *assembly_file;
 FILE *object_file;
 
-typedef struct source_token
-{
-	char *token;
-	int size;
-}
-token;
+node *labels;		//linked list of labels
+token *lines;		//array of line tokens
+
 
 int main(int argc, char *argv[])
 {
 	char 	c,
-		*buffer,
-		*program;
-	int 	num_tokens = 1,
-		program_index = 0,
-		buffer_size = 1;
-		buffer_index = 0;
-		i,j,k;
-//	token 	*program;
+		*buffer = NULL,
+		*assembly_code = NULL;
+	void	*temporary_ptr = NULL;
+	
+	int 	code_size 	= 1,
+		code_index 	= 0,
+		buffer_size 	= 1,
+		buffer_index 	= 0,
+		num_line_tokens	= 1,
+		line_index 	= 0;
+
+	uint16_t input_address	= 0;
 
 	//obtain asm and object filenames
 	if(argc < 3)
@@ -352,38 +369,111 @@ int main(int argc, char *argv[])
 	assembly_file = fopen(argv[1], "r");
 	object_file = fopen(argv[2], "w");
 
-	//receive and store tokens from assembly source
-	program = malloc(sizeof(char));
-//	buffer = malloc(sizeof(char));
+	//receive and store code from assembly source
+	assembly_code = malloc(sizeof(char));
 
 	while((c = fgetc(assembly_file)) != EOF)
 	{
-		program[index] = c;
-		printf("%4x\n", program[index]);		
+		assembly_code[code_index] = c;
+		printf("%c", assembly_code[code_index]);		
 
-		if
-
-		if((program = realloc(program, (size + 1) * sizeof(char)) == NULL)
+		//if realloc fails, exit
+		temporary_ptr = assembly_code;
+		if((assembly_code = realloc(assembly_code, (code_size + 1) * sizeof(char))) == NULL)
 		{
-			printf("Assembly Failed. Please try again.\n");
+			printf("No more memory to store line token. Please try again.\n");
+			free(temporary_ptr);
 			exit(0);
 		}
 
-		index++;
-		size++;
+		code_index++;
+		code_size++;
 	}
 
-	//Find labels
-	index = 0;
+	//process code-buffer to produce array of strings of line tokens and linked list of labels
+	code_index = 0;
 
-	while(index < size)
+	lines = malloc(sizeof(token));
+	buffer = malloc(sizeof(char));
+	
+	while(code_index < code_size)
 	{
+		c = assembly_code[code_index];
+	
+		//if buffer contains label
+		if(c == ':')
+		{
+			AddLabelNode(buffer, input_address + (uint16_t)line_index, &labels);			
+			printf("Label: %s\n", (labels -> last_node) -> label);
+	
+			//reset buffer
+			temporary_ptr = buffer;
+			if((buffer = realloc(buffer, sizeof(char))) == NULL)
+			{
+				printf("No more memory to store program. Please try again.\n");
+				free(temporary_ptr);
+				exit(0);
+			}		
+
+			buffer_index = 0;
+			buffer_size = 1;
+
+			code_index++;
+			continue;
+		}
+		
+		//if at end of line
+		if(c == NEWLINE || c == ';')
+		{
+			//create new line token
+			lines[line_index].token = buffer;
+			lines[line_index].token_size = buffer_size;
 			
+			printf("Line Token #%i: %s\n", line_index, lines[line_index].token);
+
+			//add new token struct
+			temporary_ptr = lines;
+			if((lines = realloc(lines, (num_line_tokens + 1) * sizeof(token))) == NULL)
+			{
+				printf("No more memory to store program. Please try again.\n");
+				free(temporary_ptr);
+				exit(0);
+			}
+			lines[line_index + 1].token = NULL; 
+
+			line_index += 1;
+			num_line_tokens += 1;			
+
+			//reset buffer
+			buffer = malloc(sizeof(char));
+			buffer_index = 0;
+			buffer_size = 1;
+	
+			//if ; then go to new line	
+			while(c == ';' && assembly_code[code_index] != NEWLINE)
+{
+	code_index += 1;
+} 
+	
+			code_index++;
+			continue;
+		}  		
+
+		buffer[buffer_index] = c;	
+		buffer_index++;
+		buffer_size++;
+
+		code_index++;
 	}
 
+	//process operands
+
+	//free memory
+	
 	return 1;
 }
 
+/*
 if(c == ' ' || c == ',' && buffer_size > 0)
 {
 	program[program_index].token = buffer;
@@ -398,4 +488,4 @@ if(c == ' ' || c == ',' && buffer_size > 0)
 	
 	num_tokens += 1;
 }
-
+*/
