b999075f923228c449ad91bf39c7b2828b4360cd
Pramuka Perera <pramuka.perera@gmail.com>
2017-08-27 22:14:00 -0400

    Commit in order to merge branch

--- write_to_storage.asm
+++ write_to_storage.asm
@@ -1,22 +1,55 @@
+LETTER 	EQU 42
+ADRL	EQU 01
+ADRH	EQU 00
+ADR_L	EQU 01
+ADR_H	EQU 00
+
+INTE	EQU 01
+RDY	EQU 02
+READ	EQU 04
+WRITE	EQU 08
+DONE	EQU 10
+RESET	EQU 0F
+
+CTRL	EQU 3ffc
+DATA	EQU 3ffd
+M_ADR_L	EQU 3ffe
+M_ADR_H	EQU 3fff
+
+;BITS	4	3	2	1	0
+;FLAGS	DONE	WRITE	READ	RDY	INTE
+
 ;Store data to nv data register
-MVI A, 41
-STA 3ffd
+MVI A, LETTER
+STA DATA
 
 ;Store address to address register
-MVI A, 01
-STA 3ffe
+MVI A, ADRL
+STA M_ADR_L
 
-MVI A, 00
-STA 3fff
+MVI A, ADRH
+STA M_ADR_H
 
 ;Modify control register to write to storage
-LDA 3ffc
-ORI 08
-STA 3ffc
+LDA CTRL
+ORI WRITE
+STA CTRL
 
 ;Wait for storage to happen
-WAIT: 	LDA 3ffc
-	ANI 02
-	JZ WAIT
+SWAIT: 	LDA CTRL
+	ANI DONE	
+	JZ SWAIT
 
-HLT 
+;Wait for reset to ready state
+RWAIT:	LDA CTRL
+	MOV B, A
+	ANI RDY
+	JNZ FIN
+ 
+	ANI RESET
+	STA CTRL
+	JMP RWAIT
+
+FIN:	HLT
+
+END 
